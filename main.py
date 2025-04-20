efedc2f4abfb185ebd86e0b1a2459f7ad55668c2
unrulytoast <banannapancake42@gmail.com>
2025-04-20 14:21:54 -0500

    added function to count all the characters in a dictionary

--- main.py
+++ main.py
@@ -2,12 +2,10 @@
 def main():
     path_to_file = "books/frankenstein.txt"
     print(f"{get_word_count(path_to_file)} words found in the document")
-    #return get_word_count(path_to_file)
-    #print(get_book_text(path_to_file))
-    #return get_book_text(path_to_file)
+    print(get_char_count(path_to_file))
 
 from stats import get_word_count
-
+from stats import get_char_count
 
 main()
 
--- stats.py
+++ stats.py
@@ -7,3 +7,21 @@ def get_word_count(path_to_file):
     for w in words:
         word_count += 1
     return word_count
+
+#This returns the character count as a dictionary
+def get_char_count(path_to_file):
+    with open(path_to_file) as f:
+        file_contents = f.read()
+    characters =  file_contents
+    char_dict = {}
+    for c in characters:
+        c = c.lower()
+        if c in char_dict:
+            char_dict[c] += 1 
+        else:
+            char_dict[c] = 1
+    return char_dict
+
+    
+
+
